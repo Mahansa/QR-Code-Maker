184dad89bd0eb9aa6dec34513723bc5d257a7245
Mahansa <hansagamers11@gmail.com>
2025-07-16 14:38:55 +0700

    Add some instruction

--- qr_code.py
+++ qr_code.py
@@ -1,9 +1,9 @@
 import qrcode
-website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+website_link = 'Paste your link in here' #change with your own link
 
 qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data(website_link)
 qr.make()
 
 img = qr.make_image(fill_color = 'black', back_color = 'white')
-img.save('youtube_qr.png')
+img.save('yourImageName.png')#you can change the name of the .png
